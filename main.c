562cd2f4191887e4362ef96f6b3b194b8fb2756b
kungyu <hismayfly@gmail.com>
2021-05-26 00:10:33 +0900

    check ip address

--- main.c
+++ main.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 int
-main(int argc, char *argv[])
+main(void)
 {
 	int socket_descriptor;
+	struct sockaddr_in server;
+
 	socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (socket_desc == -1)
+	if (socket_descriptor == -1)
 	{
-		printf("Could not create socket");
+		printf("Could not create socket\n");
+	}
+	printf("Created socket\n");
+
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 80 );
+
+	printf("Ready to connect\n");
+	if (connect(socket_descriptor, (struct sockaddr *)&server, sizeof(server)) < 0)
+	{
+		puts("Connect error");
+		return 1;
 	}
 
+	puts("Connected");
 	return 0;
 }
