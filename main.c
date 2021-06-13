c8a14a882fe242d06c66043ab334353bd1a0132b
kungyu <hismayfly@gmail.com>
2021-06-13 20:41:11 +0900

    tcp client

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
@@ -7,6 +8,7 @@ main(void)
 {
 	int socket_descriptor;
 	struct sockaddr_in server;
+	char *message, server_reply[2000];
 
 	socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -16,7 +18,7 @@ main(void)
 	}
 	printf("Created socket\n");
 
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_addr.s_addr = inet_addr("142.250.204.46");
 	server.sin_family = AF_INET;
 	server.sin_port = htons( 80 );
 
@@ -28,5 +30,22 @@ main(void)
 	}
 
 	puts("Connected");
+
+	message = "GET / HTTP/1.1\r\n\r\n";
+	if ( send(socket_descriptor, message, strlen(message), 0) < 0)
+	{
+		puts("Send failed");
+		return 1;
+	}
+	puts("Data Sent\n");
+
+	if (recv(socket_descriptor, server_reply, 2000, 0) < 0)
+	{
+		puts("Recv failed");
+	}
+
+	puts("Reply received\n");
+	puts(server_reply);
+
 	return 0;
 }
